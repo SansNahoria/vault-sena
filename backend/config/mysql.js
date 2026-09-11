daa750d9ba1af77c6cab9b6e4f4014f81585c04d
SansNahoria <jaun20002@gmail.com>
2026-09-11 11:44:37 -0500

    Update mysql.js

--- backend/config/mysql.js
+++ backend/config/mysql.js
@@ -10,12 +10,16 @@ require('dotenv').config();
 
 const pool = mysql.createPool({
   host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
   connectionLimit: 10, // máximo de conexiones simultáneas
-  queueLimit: 0
+  queueLimit: 0,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // Función para probar que la conexión funciona al iniciar el servidor
